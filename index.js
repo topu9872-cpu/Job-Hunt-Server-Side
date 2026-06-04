7aa2af464294d2380fecbd729f45f50079420ffd
topu9872-cpu <topu9872@gmail.com>
2026-06-04 12:10:44 +0600

    coneceted with database

---
+++ index.js
@@ -0,0 +1,77 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express())
+
+const port = 5000
+
+
+
+
+
+const uri = process.env.MONGODB_URI ;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+ const db= client.db('Job_Hunt');
+    const jobCollections = db.collection('job_data');
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
